6bec4561892e16984a18bc4757515695e2e9ee27
Surbhi Kumari <77582783+surbhi400@users.noreply.github.com>
2023-01-04 21:55:35 +0530

    04-01-23

---
+++ 2023 leetcode questions/Minimum Rounds to Complete All Tasks.cpp
@@ -0,0 +1,49 @@
+/* You are given a 0-indexed integer array tasks, where tasks[i] represents the difficulty level of a task. In each round, you can complete either 2 or 3 tasks of 
+the same difficulty level.
+
+Return the minimum rounds required to complete all the tasks, or -1 if it is not possible to complete all the tasks.
+
+ 
+
+Example 1:
+
+Input: tasks = [2,2,3,3,2,4,4,4,4,4]
+Output: 4
+Explanation: To complete all the tasks, a possible plan is:
+- In the first round, you complete 3 tasks of difficulty level 2. 
+- In the second round, you complete 2 tasks of difficulty level 3. 
+- In the third round, you complete 3 tasks of difficulty level 4. 
+- In the fourth round, you complete 2 tasks of difficulty level 4.  
+It can be shown that all the tasks cannot be completed in fewer than 4 rounds, so the answer is 4. */
+
+/*  If the frequency is 1, the task cannot be performed so we return -1.
+If the frequency is 3, the task can be performed and we return frequency/3.
+Now if the frequency is not completely divisible by 3, then the possible remainders are 1 and 2. Lets check them out:
+If remainder is 1, i.e, lets take 7 frequency, then 🔮🔮🔮🔮🔮🔮🔮 the groups are something like this "🔮🔮🔮" "🔮🔮🔮" "🔮", i.e, one 🔮 is left alone so the
+solution is to break the second last group into two parts of 2 and 1 so that it becomes like: "🔮🔮🔮" "🔮🔮" "🔮🔮", i.e, one is added to the division by 3.
+If remainder is 2, i.e, lets take 8 frequency, then 🔮🔮🔮🔮🔮🔮🔮🔮 the groups are something like this "🔮🔮🔮" "🔮🔮🔮" "🔮🔮", i.e, two 🔮 are left so the
+solution is to just add 1,i.e, one group of two to the solution. /*
+----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+  class Solution {
+public:
+    int minimumRounds(vector<int>& tasks) {
+        unordered_map<int,int> mp;
+        for(int i=0;i<tasks.size();i++){
+            mp[tasks[i]]++;
+        }
+        
+        int output=0;
+        
+        for (auto freq : mp){
+            if(freq.second==1)
+                return -1;
+            if(freq.second%3==0){
+                output += freq.second/3;
+            }
+            else{
+                output += freq.second/3 + 1;
+            }
+        }
+        return output;
+    }
+};
