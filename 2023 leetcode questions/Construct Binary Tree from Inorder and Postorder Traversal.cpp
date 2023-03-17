66b37b9888bedc425416e43ad7bd0b2020d4d9e4
Surbhi Kumari <77582783+surbhi400@users.noreply.github.com>
2023-03-17 23:42:30 +0530

    16-03-23

--- 2023 leetcode questions/Construct Binary Tree from Inorder and Postorder Traversal.cpp
+++ 2023 leetcode questions/Construct Binary Tree from Inorder and Postorder Traversal.cpp
@@ -6,7 +6,7 @@ Example 1:
 
 
 Input: inorder = [9,3,15,20,7], postorder = [9,15,7,20,3]
-Output: [3,9,20,null,null,15,7]
+Output: [3,9,20,null,null,15,7]  
 Example 2:
 
 Input: inorder = [-1], postorder = [-1]
